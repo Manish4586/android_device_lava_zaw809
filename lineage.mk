6d2315a0541954f2123312d0308497dc802caf42
Manish4586 <Manish.n.manish45@gmail.com>
2018-01-01 18:58:13 +0530

    viper.mk

--- lineage.mk
+++ lineage.mk
@@ -2,17 +2,17 @@
 PRODUCT_RELEASE_NAME := irisX8
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/viper/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/Lava/IrisX8/device_IrisX8.mk)
 
 # Locales
-PRODUCT_LOCALES := en_US 
+PRODUCT_LOCALES := en_US en_IN fr_FR it_IT es_ES
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := irisX8
-PRODUCT_NAME := lineage_IrisX8
+PRODUCT_NAME := viper_IrisX8
 PRODUCT_BRAND := iris
 PRODUCT_MODEL := Iris X8 N
 PRODUCT_MANUFACTURER := Lava
