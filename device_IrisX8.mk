5f54d1074f2fd8ed2c49f2b1e7760424de2be3de
Manish4586 <Manish.n.manish45@gmail.com>
2018-01-16 23:48:06 +0530

    Initial Commit

--- device_IrisX8.mk
+++ device_IrisX8.mk
@@ -62,7 +62,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/init.mt6592.usb.rc:root/init.mt6592.usb.rc \
     $(LOCAL_PATH)/rootdir/init.recovery.mt6592.rc:/init.recovery.mt6592.rc \
     $(LOCAL_PATH)/rootdir/ueventd.mt6592.rc:root/ueventd.mt6592.rc \
-    $(LOCAL_PATH)/rootdir/ueventd.rc:root/ueventd.rc \
     $(LOCAL_PATH)/rootdir/twrp.fstab:recovery/root/etc/twrp.fstab \
     $(LOCAL_KERNEL):kernel
 
