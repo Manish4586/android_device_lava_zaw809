49570ee98e28fdd8ab95dd8b2503b857de8ab87e
Manish4586 <Manish.n.manish45@gmail.com>
2018-01-16 23:22:09 +0530

    Fix cam : experimental

--- libmtk_symbols/mtk_cam.cpp
+++ libmtk_symbols/mtk_cam.cpp
@@ -2,6 +2,6 @@ extern "C" void _ZN7android9SingletonINS_13SensorManagerEE9sInstanceE() { }
 extern "C" void _ZN7android9SingletonINS_13SensorManagerEE5sLockE() { }
 extern "C" void _ZN7android13SensorManagerC1Ev() { }
 extern "C" void _ZN7android13SensorManager16createEventQueueEv() { }
-
+extern "C" void _ZN9NSCamShot10IMultiShot14createInstanceEN5NSCam9EShotModeEPKc() { }
 
 
