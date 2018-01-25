3595a453c4645bed7d090d97591c0cc9ffa4ecc7
Manish4586 <Manish.n.manish45@gmail.com>
2018-01-25 12:37:41 +0530

    update

--- aosp_IrisX8.mk
+++ aosp_IrisX8.mk
@@ -2,7 +2,7 @@
 PRODUCT_RELEASE_NAME := irisX8
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/Lava/IrisX8/device_IrisX8.mk)
@@ -12,7 +12,7 @@ PRODUCT_LOCALES := en_US en_AU en_IN fr_FR it_IT es_ES et_EE de_DE nl_NL cs_CZ p
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := IrisX8
-PRODUCT_NAME := lineage_IrisX8
+PRODUCT_NAME := aosp_IrisX8
 PRODUCT_BRAND := iris
 PRODUCT_MODEL := Iris X8 N
 PRODUCT_MANUFACTURER := Lava
