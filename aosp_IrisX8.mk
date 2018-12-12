044358ad449391377bff9503c3f89b3338f23f56
Manish4586 <Manish.n.manish45@gmail.com>
2018-12-13 03:34:00 +0530

    Update aosp_IrisX8.mk

--- aosp_IrisX8.mk
+++ aosp_IrisX8.mk
@@ -2,7 +2,7 @@
 PRODUCT_RELEASE_NAME := irisX8
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/Lava/IrisX8/device_IrisX8.mk)
